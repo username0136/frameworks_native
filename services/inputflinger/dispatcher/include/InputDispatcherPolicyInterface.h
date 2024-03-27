149247e7799e33f9906fe67ec424e49ae5054074
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-27 12:12:43 +0100

    Revert "InputDispatcher: On keypress, deliver keycode to pokeUserActivity"
    This reverts commit 60c66aae948584048bda95eecafb6a029bc27339.

--- services/inputflinger/dispatcher/include/InputDispatcherPolicyInterface.h
+++ services/inputflinger/dispatcher/include/InputDispatcherPolicyInterface.h
@@ -119,8 +119,7 @@ public:
                               uint32_t policyFlags) = 0;
 
     /* Poke user activity for an event dispatched to a window. */
-    virtual void pokeUserActivity(nsecs_t eventTime, int32_t eventType, int32_t displayId,
-                                  int32_t keyCode) = 0;
+    virtual void pokeUserActivity(nsecs_t eventTime, int32_t eventType, int32_t displayId) = 0;
 
     /*
      * Return true if the provided event is stale, and false otherwise. Used for determining
